7c2ec74b265c7369b9279d886dc1f6fdaec635f4
Anna Łopusińska <am.lopusinska@gmail.com>
2019-05-24 22:52:45 +0200

    3 podpunkt zadania z butelkami

--- dareITtesty/lib/bottle.rb
+++ dareITtesty/lib/bottle.rb
@@ -1,12 +1,19 @@
 class Bottles
 
 def verse(number)
+
+ if number==2
+   "#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number-1} bottle of beer on the wall."
+else
   "#{number} bottles of beer on the wall, #{number} bottles of beer.
 Take one down and pass it around, #{number-1} bottles of beer on the wall."
+
+  end
 end
 
 end
 
-# 2. After running 'Bottles.new.verse(89)' I'm expecting to get:
-# '89 bottles of beer on the wall, 89 bottles of beer.
-#  Take one down and pass it around, 88 bottles of beer on the wall.'
+# 3. After running 'Bottles.new.verse(2)' I'm expecting to get:
+# '2 bottles of beer on the wall, 2 bottles of beer.
+#  Take one down and pass it around, 1 bottle of beer on the wall.'
--- dareITtesty/spec/bottle_spec.rb
+++ dareITtesty/spec/bottle_spec.rb
@@ -16,9 +16,14 @@ Take one down and pass it around, 98 bottles of beer on the wall.'
     expect(Bottles.new.verse(89)).to eq '89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.'
   end
+
+  it 'verse2' do
+  expect(Bottles.new.verse(2)).to eq '2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.'
+  end
 end
 
 
-# 2. After running 'Bottles.new.verse(89)' I'm expecting to get:
-# '89 bottles of beer on the wall, 89 bottles of beer.
-#  Take one down and pass it around, 88 bottles of beer on the wall.'
+# 3. After running 'Bottles.new.verse(2)' I'm expecting to get:
+# '2 bottles of beer on the wall, 2 bottles of beer.
+#  Take one down and pass it around, 1 bottle of beer on the wall.'
