4f601a76dad082cfc5b3658f6cebdbf700fbe00c
Anna Łopusińska <am.lopusinska@gmail.com>
2019-05-24 23:02:23 +0200

    5 podpunkt z butelkami

--- dareITtesty/lib/bottle.rb
+++ dareITtesty/lib/bottle.rb
@@ -8,6 +8,9 @@ Take one down and pass it around, #{number-1} bottle of beer on the wall."
 elsif number==1
   "1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall."
+elsif number==0
+  "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
 else
   "#{number} bottles of beer on the wall, #{number} bottles of beer.
 Take one down and pass it around, #{number-1} bottles of beer on the wall."
@@ -17,6 +20,6 @@ end
 
 end
 
-# 4. After running 'Bottles.new.verse(1)' I'm expecting to get:
-# '1 bottle of beer on the wall, 1 bottle of beer.
-#  Take it down and pass it around, no more bottles of beer on the wall.'
+# 5. After running 'Bottles.new.verse(0)' I'm expecting to get:
+# 'No more bottles of beer on the wall, no more bottles of beer.
+#  Go to the store and buy some more, 99 bottles of beer on the wall.'
--- dareITtesty/spec/bottle_spec.rb
+++ dareITtesty/spec/bottle_spec.rb
@@ -27,9 +27,14 @@ Take one down and pass it around, 1 bottle of beer on the wall.'
 Take it down and pass it around, no more bottles of beer on the wall.'
   end
 
+  it 'verse0' do
+  expect(Bottles.new.verse(0)).to eq 'No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.'
+  end
+
 end
 
 
-# 4. After running 'Bottles.new.verse(1)' I'm expecting to get:
-# '1 bottle of beer on the wall, 1 bottle of beer.
-#  Take it down and pass it around, no more bottles of beer on the wall.'
+# 5. After running 'Bottles.new.verse(0)' I'm expecting to get:
+# 'No more bottles of beer on the wall, no more bottles of beer.
+#  Go to the store and buy some more, 99 bottles of beer on the wall.''
